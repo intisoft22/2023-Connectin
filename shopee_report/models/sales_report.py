a97849b88e124116dd08009f8fb72d0ca1c34926
mifta <54034489+mifta99@users.noreply.github.com>
2023-06-07 11:08:19 +0700

    set modal = qty*cost product

--- shopee_report/models/sales_report.py
+++ shopee_report/models/sales_report.py
@@ -98,14 +98,22 @@ class SalesReport(models.Model):
                 filter_account = account.search([('invoice_date', '=', ac_date), ('payment_state', '=', 'paid')])
 
                 sum_amount_total = 0
+                sum_modal = 0
                 for fa in filter_account:
                     sale = self.env['sale.order'].search([('client_order_ref', '=', fa.ref), ('marketplace_account_id', '=', rec.marketplace_id.id)])
 
                     if sale:
                         sum_amount_total += fa.amount_total
 
+                        for aml in fa.invoice_line_ids:
+                            product = self.env['product.product'].search([('id', '=', aml.product_id.id)])
+
+                            temp_modal = product.standard_price * aml.quantity
+
+                            sum_modal += temp_modal
+
                 disc = 0
-                modal_jual = 0
+                modal_jual = sum_modal
                 biaya_admin = 0
                 net = sum_amount_total - modal_jual - disc - biaya_admin
 
